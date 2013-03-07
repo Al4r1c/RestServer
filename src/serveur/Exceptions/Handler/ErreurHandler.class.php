f1c6806335d19f02d65b3f0028287dd443ff9d41
Yann KURDIJAKA <alaricz@gmail.com>
2013-03-07 10:27:01 +0100

    Correctif: Checkstyle et Erreur Handler (bad indent)

--- src/serveur/Exceptions/Handler/ErreurHandler.class.php
+++ src/serveur/Exceptions/Handler/ErreurHandler.class.php
@@ -85,7 +85,7 @@
                     $erreur = new Error($codeErreur);
                     $erreur->setMessage(
                         '{trad.file}: ' . $fichierErreur . ', {trad.line}: ' . $ligneErreur . ' | {trad.warning}: ' .
-                            $messageErreur);
+                        $messageErreur);
                     $this->ecrireErreur($erreur);
                     throw new \Exception();
                     break;
@@ -103,7 +103,7 @@
                     $erreur = new Notice($codeErreur);
                     $erreur->setMessage(
                         '{trad.file}: ' . $fichierErreur . ', {trad.line}: ' . $ligneErreur . ' | {trad.warning}: ' .
-                            $messageErreur);
+                        $messageErreur);
                     $this->ecrireErreur($erreur);
                     break;
 
