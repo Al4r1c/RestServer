86f69066c3089f765d0f11a903a5a04c73da755d
Yann KURDIJAKA <alaricz@gmail.com>
2013-03-18 12:13:10 +0100

    Modification de répertoire

--- src/serveur/Traitement/Data/DatabaseFactory.class.php
+++ src/serveur/Traitement/Data/DatabaseFactory.class.php
@@ -9,8 +9,8 @@
          */
         public function getConnexionDatabase($nomDriverDatabase)
         {
-            if (class_exists(
-                $nomClasseDatabase = '\\Serveur\\Traitement\\Data\\Database' . ucfirst(strtolower($nomDriverDatabase)))
+            if (class_exists($nomClasseDatabase =
+                '\\Serveur\\Traitement\\Data\\Drivers\\Database' . ucfirst(strtolower($nomDriverDatabase)))
             ) {
                 return new $nomClasseDatabase();
             } else {
