1a576072778a3f80f2288efb7decabb533564e2f
Yann KURDIJAKA <alaricz@gmail.com>
2013-03-19 11:11:35 +0100

    Correctif style #2

--- src/serveur/Traitement/Data/DatabaseFactory.class.php
+++ src/serveur/Traitement/Data/DatabaseFactory.class.php
@@ -9,9 +9,10 @@
          */
         public function getConnexionDatabase($nomDriverDatabase)
         {
-            if (class_exists(
-                $nomClasseDatabase =
-                    '\\Serveur\\Traitement\\Data\\Drivers\\Database' . ucfirst(strtolower($nomDriverDatabase)))
+            if (
+                class_exists($nomClasseDatabase =
+                '\\Serveur\\Traitement\\Data\\Drivers\\Database' . ucfirst(strtolower($nomDriverDatabase))
+                )
             ) {
                 return new $nomClasseDatabase();
             } else {
