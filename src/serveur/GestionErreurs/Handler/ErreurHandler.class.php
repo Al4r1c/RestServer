e8650996b8a790aefd4e538cfc1a2fefc1c52a85
Yann KURDIJAKA <alaricz@gmail.com>
2013-03-19 15:21:25 +0100

    Correction de la récupération des arguments#2 (erreur de signe)

--- src/serveur/GestionErreurs/Handler/ErreurHandler.class.php
+++ src/serveur/GestionErreurs/Handler/ErreurHandler.class.php
@@ -58,7 +58,7 @@ class ErreurHandler
      */
     public function global_ajouterErreur($erreurNumber, $codeErreur)
     {
-        $arguments = array_slice(func_get_args(), -2);
+        $arguments = array_slice(func_get_args(), 2);
 
         switch ($erreurNumber) {
             case E_USER_ERROR:
