ff2a576044b0a2f5ace8689501c034d49a7223bb
Yann KURDIJAKA <alaricz@gmail.com>
2013-03-21 11:17:56 +0100

    Modif de style#2421623....

--- src/logging/Displayer/Logger.class.php
+++ src/logging/Displayer/Logger.class.php
@@ -98,7 +98,7 @@ class Logger extends AbstractDisplayer
         $this->_fichierLogAcces->ecrireDansFichier(
             "\t" . $this->traduireMessageEtRemplacerVariables(
                 "{trad.reponseCode}: " . $objetReponse->getStatusHttp() . " - {trad.reponseFormat}: " .
-                    $objetReponse->getFormat()
+                $objetReponse->getFormat()
             ) . "\n"
         );
     }
@@ -132,7 +132,7 @@ class Logger extends AbstractDisplayer
         $this->_fichierLogErreur->ecrireDansFichier($uneErreur->getDate()->format('d-m-Y H:i:s') . ": \n");
         $this->_fichierLogErreur->ecrireDansFichier(
             "\t" . $this->traduireMessageEtRemplacerVariables(
-                "{trad.error}" . " n°" . $uneErreur->getCodeErreur() . ": {errorType." . $errorType . "}\n"
+            "{trad.error}" . " n°" . $uneErreur->getCodeErreur() . ": {errorType." . $errorType . "}\n"
             )
         );
 
